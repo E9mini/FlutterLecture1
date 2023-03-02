f86ac02156f9c26a4801e3eda4e1e48cf1aa26db
Nikm-Lee <kmdiabolos@naver.com>
2023-03-02 23:28:29 +0900

    앱바 메뉴

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(rootViewController());
+void main() => runApp(MyApp());
 
-class rootViewController extends StatelessWidget {
-  const rootViewController({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "E9mini",
-      home: Grade(),
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+  // var accountName = "E9mini";
+  // var accountEmail = "lkm.es@esgroup.net";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[800],
       appBar: AppBar(
-        title: Text("NIKM"),
-        backgroundColor: Colors.blue[700],
         centerTitle: true,
+        title: Text("AppBar icon menu"),
         elevation: 0.0,
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('Shopping cart button is clicked');
+              },
+              icon: Icon(Icons.shopping_cart)),
+          IconButton(
+              onPressed: () {
+                print('SearchButton is clicked');
+              },
+              icon: Icon(Icons.search))
+        ],
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/cat-icon2.png'),
                 backgroundColor: Colors.white,
-                backgroundImage: AssetImage(
-                  'assets/cat-icon.png',
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/cat-icon.png'),
+                  backgroundColor: Colors.white,
                 ),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 1.5,
-              endIndent: 30.0,
-            ),
-            Text(
-              "NAME",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '즐겨찾기',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "즐겨찾기 POWER LEVEL",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '14',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'using lightsaber',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/cat-icon2.png'),
+                  backgroundColor: Colors.white,
                 ),
               ],
+              accountName: Text("이규민"),
+              accountEmail: Text("lkm.es@esgroup.net"),
+              onDetailsPressed: () {
+                print("arrow is clicked");
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(40.0),
+                    bottomLeft: Radius.circular(40.0),
+                  )),
             ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'Face hero tatto',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'fire falmes',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundColor: Colors.blue[800],
-                backgroundImage: AssetImage("assets/cat-icon2.png"),
-                radius: 40.0,
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
               ),
+              title: Text("Home"),
+              onTap: () {
+                print("Home is clicked");
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text("Setting"),
+              onTap: () {
+                print("Setting is clicked");
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text("Q&A"),
+              onTap: () {
+                print("Q&A is clicked");
+              },
+              trailing: Icon(Icons.add),
             ),
           ],
         ),
